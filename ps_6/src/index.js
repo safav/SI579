abc4b83a33943e101c7499cdf5c7fc1e32df3840
safav <70871471+safav@users.noreply.github.com>
2021-03-05 19:47:22 -0500

    ps 6

---
+++ ps_6/src/index.js
@@ -0,0 +1,25 @@
+/*
+I am using anime.js to create an animated text effect when the page loads as a intro.
+I also used moment.js to help figure out when a cake would arrive if a order were placed on a certain day.
+In addition I use bootstrap to help style the website.
+*/
+import moment from 'moment';
+import anime from 'animejs/lib/anime.es.js';
+
+
+let textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [-100, 0],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: (el, i) => 30 * i
+    })
+let input = document.getElementById("exampleFormControlInput1");
+input.addEventListener("change", (e) => {
+    document.getElementById("result").innerText = "It will arrive " + moment(e.target.value).add(10, 'days').calendar();
+})
+
